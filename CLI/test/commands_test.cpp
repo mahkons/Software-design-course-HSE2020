01f26c46bdb6006c738dbe5d47fc3e4683c1aa14
Makhnev Konstantin <mah.kons@gmail.com>
2020-01-26 23:15:45 +0300

    add empty test files for travis

---
+++ CLI/test/commands_test.cpp
@@ -0,0 +1,16 @@
+#include <sstream>
+#include <memory>
+
+#include "gtest/gtest.h"
+
+#include "commands/command.h"
+
+namespace {
+    using namespace NCLI;
+    using namespace NCLI::NCommand;
+
+
+
+} // namespace
+
+
---
+++ CLI/test/parser_test.cpp
@@ -0,0 +1,17 @@
+#include <sstream>
+#include <memory>
+
+#include "gtest/gtest.h"
+
+#include "commands/command.h"
+#include "parser/parser.h"
+
+namespace {
+    using namespace NCLI;
+    using namespace NCLI::NCommand;
+    using namespace NCLI::NParser;
+
+
+
+} // namespace
+
