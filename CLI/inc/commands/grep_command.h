a81b6e46011b9c3fe83768dc232970b656bb3429
Makhnev Konstantin <mah.kons@gmail.com>
2020-02-11 17:26:03 +0300

    Add comments

--- CLI/inc/commands/grep_command.h
+++ CLI/inc/commands/grep_command.h
@@ -21,20 +21,34 @@ namespace NCLI::NCommand {
     class GrepCommand : public Command {
     public:
         /**
+         * Command will output lines from input which contains pattern
+         * and @param trailing_context_lines after each of them
          */
         GrepCommand(const std::regex& pattern, int trailing_context_lines)
             : pattern_(pattern), trailing_context_lines_(trailing_context_lines) {};
         /**
+         * Command will output lines from input file which contains pattern
+         * and @param trailing_context_lines after each of them
          */
-        // TODO think about adding builder
         GrepCommand(const std::string& filename, const std::regex& pattern, int trailing_context_lines)
             : filename_(std::filesystem::absolute(filename)), pattern_(pattern),
             trailing_context_lines_(trailing_context_lines) {};
+
         /**
+         * Search pattern in file if file is specified
+         * or in input stream otherwise
+         * If file is specified but cannot be open, returns result with ExecutionStatus::error
          */ 
         virtual ExecutionResult execute(std::istream& is, std::ostream& os) override;
 
         /**
+         * Parses commands args
+         * First argument should used as command name
+         * If flag -i is given, pattern will ignore case
+         * If flag -w if given, pattern will match only whole words
+         * If argument -A NUM is given, NUM trailing lines would be printed after each line
+         * First not named parameter will be used as pattern
+         * All other not named parameters will be used as input files
          */ 
         static Result<std::shared_ptr<Command>, std::string> create_command(
                 const std::vector<std::string>& args);
